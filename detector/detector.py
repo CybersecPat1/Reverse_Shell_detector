11cef33aa585a7af4e909d64d337b07a5af9aeca
CybersecPat1 <183147624+CybersecPat1@users.noreply.github.com>
2025-03-13 13:01:56 -0500

    added notification capability

--- detector/detector.py
+++ detector/detector.py
@@ -1,17 +1,24 @@
 import time
-from psutil import Process, Popen
+import ctypes
+from psutil import process_iter
 
+# Function to show a notification on Windows
+def show_notification(title, message):
+    ctypes.windll.user32.MessageBoxW(0, message, title, 1)
+
+# Function to check for new "streamreader" processes
 def check_new_streamreader():
-    # get the list of all running processes
-    processes = Process.all_processes()
+    # Get the list of all running processes
+    processes = process_iter(['name'])
 
-    # loop through each process and check if it is a streamreader process
+    # Loop through each process and check if it is a "streamreader" process
     for p in processes:
-        if "streamreader" in p.name():
-            print("New streamreader process created")
+        if "streamreader" in p.info['name']:
+            show_notification("Process Alert", "New possible reverse shell process created")
             break
 
+# Continuous loop to check every 10 seconds
 while True:
-    # wait for 10 seconds before checking again
+    # Wait for 10 seconds before checking again
     time.sleep(10)
     check_new_streamreader()
